9dd65d57cd47144b3f98e64ef8d775e06668f96b
Thiago Alessandro Batista <alebatistathiago@gmail.com>
2024-08-26 08:12:42 -0300

    refactor: separates scripts and env variables in files, also create scripts for auto configuration of fleet output

---
+++ setup_fleet.sh
@@ -0,0 +1,16 @@
+CERTIFICATE=$(awk '/-----BEGIN CERTIFICATE-----/{flag=1} flag; /-----END CERTIFICATE-----/{flag=0}' $FLEET_SERVER_ELASTICSEARCH_CA | awk '{printf "%s\\n", $0} END {print ""}' | sed 's/\\n$//')
+
+FINGERPRINT=$(openssl x509 -in $FLEET_SERVER_ELASTICSEARCH_CA -fingerprint -sha256 -noout | awk -F= '{print $2}' | sed 's/://g')
+
+curl -X PUT "https://elastic:changeme@kibana:5601/api/fleet/outputs/fleet-default-output" \
+-H "kbn-xsrf: true" \
+-H "Content-Type: application/json" \
+-d '{
+ "name": "default",
+ "type": "elasticsearch",
+ "hosts": ["https://es01:9200"],
+ "ca_trusted_fingerprint": "'"$FINGERPRINT"'",
+  "ssl": {
+    "certificate_authorities": ["'"$CERTIFICATE"'"]
+  }
+}' --cacert /certs/ca/ca.crt
